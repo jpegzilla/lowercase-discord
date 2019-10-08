6d13af61b1a08ea2f9d60f1ef6f483eeba804a0c
Jpegzilla <jpegzilla@gmail.com>
2019-10-08 13:18:20 -0400

    attempting keepalive

--- index.js
+++ index.js
@@ -66,4 +66,11 @@ client.on("message", msg => {
   }
 });
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN); // .listen(process.env.PORT || 5000);
+
+const express = require("express");
+const app = express();
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("listening on port", process.env.PORT);
+});
