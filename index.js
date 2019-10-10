587a3727f8076fcca89b1ec8cb154dfe063da28a
Jpegzilla <jpegzilla@gmail.com>
2019-10-10 11:38:55 -0400

    testing, trying to fix bugs with attachments

--- index.js
+++ index.js
@@ -16,12 +16,18 @@ client.once("ready", () => {
 });
 
 client.on("message", msg => {
-  const regex = /[A-HJ-Z]|I(?:[A-Za-z0-9])/gm;
+  const regex = /[A-HJ-Z]|I(?=[A-Za-z0-9])/gm;
   const emoteRegex = /:(?:[a-zA-Z0-9]+):/gm;
   const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gm;
+  const urlOnlyRegex = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/gm;
   let member = msg.member.user.username.toLowerCase();
+
   if (member == "lowercase") return;
   let originalMessage = msg.content;
+  let originalAttachments =
+    Array.from(msg.attachments).length > 0
+      ? { files: [Array.from(msg.attachments)[0][1].url] }
+      : null;
 
   const containsUppercase = string => {
     if (regex.test(string)) return true;
@@ -39,12 +45,18 @@ client.on("message", msg => {
   };
 
   const fixMessageCase = (member, originalMessage) => {
+    // replace inappropriate letters with x's
+    let replacedWithX = originalMessage.replace(regex, "x");
     let matches = originalMessage.match(regex);
     let newMsg = "";
+    let index = 0;
 
-    originalMessage.split("").forEach(c => {
-      if (matches.indexOf(c) >= 0) newMsg += c.toLowerCase();
-      else newMsg += c;
+    // replace x's with lowercase versions of the incorrect uppercase letters
+    replacedWithX.split("").forEach(c => {
+      if (c == "x") {
+        newMsg += matches[index].toLowerCase();
+        ++index;
+      } else newMsg += c;
     });
 
     return {
@@ -66,12 +78,36 @@ client.on("message", msg => {
 
     if (commandWithoutPrefix) handleUserCommands(commandWithoutPrefix, msg);
   } else if (isLikelyEmote(msg.content) || isLikelyUrl(msg.content)) {
-    return;
+    if (urlOnlyRegex.test(msg.content)) {
+      // only a url and nothing else
+      return;
+    } else {
+      // prevent capital letters from being sent in messages with links, but preserve url case
+      const url = msg.content.match(urlRegex)[0];
+      const messageWithoutUrl = msg.content.replace(url, "");
+      const fixedMessage = fixMessageCase(member, messageWithoutUrl).said;
+      const finalMessage = `${fixedMessage}${url}`;
+
+      // send message with optional attachments
+      msg.channel
+        .send(finalMessage, originalAttachments)
+        .then(nm => {
+          console.log("new message:", { newMessage: nm });
+          msg.delete();
+        })
+        .catch(e => console.log(e));
+    }
   } else if (containsUppercase(msg.content)) {
     let newMsg = fixMessageCase(member, originalMessage).said;
 
-    msg.channel.send(`${newMsg}`);
-    msg.delete();
+    // send message with optional attachments
+    msg.channel
+      .send(`${newMsg}`, originalAttachments)
+      .then(nm => {
+        console.log("new message:", { newMessage: nm });
+        msg.delete();
+      })
+      .catch(e => console.log(e));
   } else if (beginsWithPrefix(prefix, msg.content)) {
     // if user is trying to ask the bot to do something
     const commandWithoutPrefix = msg.content.replace(prefix, "").trim();
